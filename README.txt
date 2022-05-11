45f76cd18e3e6b4ddd7ed0ad08fbe9561750c4dd
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2022-05-11 17:40:50 -0400

    listing data in comment - almost complete

--- README.txt
+++ README.txt
@@ -5,4 +5,9 @@ https://www.ssbwiki.com/Category:Head_icons_(SSBM) for the character head sprite
 https://meleeframedata.com and its creator Mitchell Meier (as well as all of the people that helped him make that website) for the data
 used to create this project. 
 
+Limitations:
+    Values only include the strong hits of moves. 
+    Comparing weak hits is not an option as of right now, perhaps I will add some capability for that in the future, but we'll see. 
+    
+
 
--- src/main.java
+++ src/main.java
@@ -36,113 +36,10 @@ class Main extends Character{
                 else{
                     characters[index].charName = fileReader.next(); 
                 }
-                for (int i = 0; i < 4; i++){
-                    characters[index].jab1[i] = fileReader.nextInt(); 
+                for (int i = 0; i < 133; i++){
+                    characters[index].values[i] = fileReader.nextFloat(); 
                 }
-                for (int i = 0; i < 4; i++){
-                    characters[index].jab2[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].jab3[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].ftilt[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].utilt[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].dtilt[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].dashAttack[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].fsmash[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].usmash[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].dsmash[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].nair[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].fair[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].bair[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].uair[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].dair[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].neutralBGrounded[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].neutralBAerial[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].sideBGrounded[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].sideBAerial[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].upBGrounded[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].upBAerial[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].downBGrounded[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].downBAerial[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 2; i++){
-                    characters[index].standGrab[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 2; i++){
-                    characters[index].dashGrab[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].fthrow[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].bthrow[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].dthrow[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].uthrow[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 3; i++){
-                    characters[index].spotDodge[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 3; i++){
-                    characters[index].backRoll[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 3; i++){
-                    characters[index].forwardRoll[i] = fileReader.nextInt(); 
-                }
-                for (int i = 0; i < 4; i++){
-                    characters[index].airDodge[i] = fileReader.nextInt(); 
-                }
-                characters[index].weight = fileReader.nextInt(); 
-                characters[index].ffSpeed = fileReader.nextDouble(); 
-                characters[index].dashSpeed = fileReader.nextDouble(); 
-                characters[index].runSpeed = fileReader.nextDouble(); 
-                characters[index].wavedashLen = fileReader.nextInt();  // rank
-                characters[index].pldif = fileReader.nextInt(); 
-                characters[index].jumpSquat = fileReader.nextInt(); 
-                characters[index].wallJump = fileReader.nextInt(); 
+                
                 index += 1; 
             }
             fileReader.close(); 
@@ -157,129 +54,18 @@ class Main extends Character{
     public static void main(String args[]) {
         //ArrayList<Character> characters = new ArrayList<Character>(); 
         Character[] characters = new Character[26]; 
-        int x = 0; 
        /* double y = 5; 
-        System.out.println(String.format("%.0f", y));  */
+        System.out.println(String.format("%.0f", y));  for a value without decimals */
         
-        // need to create a massive array of floats NOT DOUBLES 
-
-
-
-        /* this is an absolute fucking abomination of a way to initialize variables, but I currently do not know of any other way to do it :P, will 
-        refactor in the future hopefully */
-        for (int i = 0; i < 26; i++){
+        for (int i = 0; i < 26; i++){ // initialize everything so it can be passed to the readFile method
             characters[i] = new Character(); 
             characters[i].charName = "Temp"; 
-            for (x = 0; x < 4; x++){
-                characters[i].jab1[x] = 1; 
+            for (int x = 0; x < 133; x++){
+                characters[i].values[x] = 0; 
             }
-            for (x = 0; x < 4; x++){
-                characters[i].jab2[x] = 1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].jab3[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].ftilt[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].utilt[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].dtilt[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].dashAttack[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].fsmash[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].usmash[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].dsmash[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].nair[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].fair[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].bair[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].uair[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].dair[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].neutralBGrounded[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].neutralBAerial[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].sideBGrounded[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].sideBAerial[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].upBGrounded[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].upBAerial[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].downBGrounded[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].downBAerial[x] =1; 
-            }
-            for (x = 0; x < 2; x++){
-                characters[i].standGrab[x] =1; 
-            }
-            for (x = 0; x < 2; x++){
-                characters[i].dashGrab[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].fthrow[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].bthrow[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].dthrow[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].uthrow[x] =1; 
-            }
-            for (x = 0; x < 3; x++){
-                characters[i].spotDodge[x] =1; 
-            }
-            for (x = 0; x < 3; x++){
-                characters[i].backRoll[x] =1; 
-            }
-            for (x = 0; x < 3; x++){
-                characters[i].forwardRoll[x] =1; 
-            }
-            for (x = 0; x < 4; x++){
-                characters[i].airDodge[x] =1; 
-            }
-            characters[i].weight = 1; 
-            characters[i].ffSpeed = 1.0; 
-            characters[i].dashSpeed = 1.0; 
-            characters[i].runSpeed = 1.0; 
-            characters[i].wavedashLen = 1;   // rank
-            characters[i].pldif = 1; 
-            characters[i].jumpSquat = 1;  
-            characters[i].wallJump = 1; 
         }
-
         readFile(characters);  
+     
         // so stuff will be accessed like characters.utiltEnding[0] for fox utilt end lag for example
 
         /* This array will contain all of the character data and will be passed to functions for manipulation
@@ -349,63 +135,61 @@ class Main extends Character{
 }
 
 class Character {
-    // if a character does not have a value that some sort of substitute value will be used. 
-    // 0 = strong hit dmg 
-    // 1 = Startup Frames
-    // 2 = active frames (duration)
-    // 3 = ending frames 
-
-    /* the plan is to add a file in the source files, where the program will retrieve the data in that by calling the population function, 
-    and fill out the array. I will need to manually input the data into the file, unforunately */ 
-
-    String charName = "test"; 
-    int[] jab1 = new int[4];
-    int[] jab2 = new int[4]; 
-    int[] jab3 = new int[4]; 
-    int[] ftilt = new int[4];  
-    int[] utilt = new int[4]; 
-    int[] dtilt = new int[4]; 
-    int[] dashAttack = new int[4]; 
-    int[] fsmash = new int[4]; 
-    int[] usmash = new int[4]; 
-    int[] dsmash = new int[4]; 
-    int[] nair = new int[4]; 
-    int[] fair = new int[4]; 
-    int[] bair = new int[4]; 
-    int[] uair = new int[4]; 
-    int[] dair = new int[4]; 
-    int[] neutralBGrounded = new int[4]; 
-    int[] neutralBAerial = new int[4]; 
-    int[] sideBGrounded = new int[4]; 
-    int[] sideBAerial = new int[4]; 
-    int[] upBGrounded = new int[4]; 
-    int[] upBAerial = new int[4]; 
-    int[] downBGrounded = new int[4]; 
-    int[] downBAerial = new int[4]; 
-    // for the grabs, 0 = startup, 1 = total frames
-    int[] standGrab = new int[2]; 
-    int[] dashGrab = new int[2]; 
-    int[] fthrow = new int[4]; 
-    int[] bthrow = new int[4]; 
-    int[] dthrow = new int[4]; 
-    int[] uthrow = new int[4]; 
-    /* in the following cases: 
-    0 = startup
-    1 = invuln frames (active)
-    2 = end frames */
-    int[] spotDodge = new int[3];      
-    int[] backRoll = new int[3]; 
-    int[] forwardRoll = new int[3]; 
-    int[] airDodge = new int[4]; 
-    // for air dodge, 0 = startup, 1 = active/invuln, 2 = end lag, 3 = special land lag 
-    int weight; 
-    double ffSpeed; 
-    double dashSpeed; 
-    double runSpeed; 
-    int wavedashLen; // rank
-    int pldif; // perfect ledgedash intangibility frames 
-    int jumpSquat; 
-    int wallJump; // 1 = yes, 0 = no. 
+    String charName; 
+    float[] values = new float[133]; 
 }
+    /*
+    indices for float array:
+    0 - jab1 dmg
+    1 - jab1 startup
+    2 - jab1 active
+    3 - jab1 ending
+    4 - 7 -> jab2
+    8 - 11 -> jab3
+    12 - 15 -> ftilt
+    16 - 19 -> utilt
+    20 - 23 -> dtilt
+    24 - 27 -> dashAttack
+    28 - 31 -> fsmash
+    32 - 35 -> usmash
+    26 - 29 -> dsmash
+    30 - 33 -> nair
+    34 - 37 -> fair 
+    38 - 41 -> bair
+    42 - 45 -> uair
+    46 - 49 -> dair
+    50 - 53 -> neutralBGrounded
+    54 - 57 -> neutralBAerial
+    58 - 61 -> sideBGrounded
+    52 - 65 -> sideBAerial
+    66 - 69 -> upBGrounded
+    70 - 73 -> upBAerial
+    74 - 77 -> downBGrounded
+    78 - 81 -> downBAerial
+    // for the grabs 0 = startup, 1 = total frames
+    82 - 83 -> standGrab
+    84 - 85 -> dashGrab
+    // back to normal now
+    86 - 89 -> fthrow
+    90 - 93 -> bthrow
+    94 - 97 -> dthrow
+    98 - 101 -> uthrow
+    // in the following: 0 = startup, 1 = invuln (Active), 2 = end
+    102 - 104 -> spotDodge
+    105 - 107 -> backRoll
+    108 - 110 -> forwardroll
+    // for airdodge, 0 = startup, 1 = active, 2 = end, 3 = special land lag 
+    111 - 114 -> airDodge
+    115 -> weight
+    116 -> ffSpeed
+    117 -> dashSpeed
+    118 -> runSpeed
+    119 -> wavedashLen // rank
+    120 -> pldif // perfect ledgefash intangibility frames
+    121 -> jumpSquat
+    123 -> wallJump // 1 yes, 0 no. 
+    */
+    
+
 
 
