75aeca939965a6fd6bc5081526f1364294ecb419
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2022-12-15 12:25:02 -0500

    file reading updated, still need to test it

--- src/FileReader.java
+++ src/FileReader.java
@@ -15,7 +15,33 @@ public class FileReader {
 
     public void readCharData(Character[] characters){
         for (Character current : characters){
-            
+            current.characterName = br.nextLine();  // name 
+            variableLoop(current.jabs, 3, 4);  // jabs 
+            variableLoop(current.tilts, 3, 4); // tilts
+            for (int i = 0; i < 4; i++){ // READ HERE BECAUSE 1D ARRAY     // dash attack
+                current.dashAttack[i] = Integer.parseInt(br.nextLine()); 
+            } 
+            variableLoop(current.smashAttacks, 3, 4);     // smash attacks
+            variableLoop(current.aerials, 5, 4);    // aerials
+            variableLoop(current.specialAttacksGrounded, 5, 4);     // specialAttacksGroudned
+            variableLoop(current.specialAttacksAerial, 5, 4);     // specialATtacksAerial
+            variableLoop(current.grabs, 2, 2);    // grabs
+            variableLoop(current.grabThrows, 4, 3);    // grabthrows
+            variableLoop(current.dodgesRolls, 4, 4);     // dodges & rolls
+            for (int i = 0; i < 8; i++){// READ HERE BECAUSE FLOATS     // misc
+                current.misc[i] = Float.parseFloat(br.nextLine()); 
+            }
+
         }
     }
+
+
+    private void variableLoop(int[][] array, int outer, int inner){
+        for (int i = 0; i < inner; i++){
+            for (int x = 0; x < outer; x++){
+                array[i][x] = Integer.parseInt(br.nextLine()); 
+            }
+        }
+    }
+    
 }
